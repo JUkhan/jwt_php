a76e3711f6b7b808432636278d94e29a838af009
JUkhan <jasim.uddin.khan@gmail.com>
2015-08-13 02:41:07 +0600

    auth

--- Scripts/jwt_ui/JwtGrid.js
+++ Scripts/jwt_ui/JwtGrid.js
@@ -198,8 +198,8 @@ var JwtGrid = React.createClass({displayName: "JwtGrid",
     	)
   },
   render: function() {
-    var options=this.props.options;     
-    if(!(this.props.data|| this.state.data)){
+    var options=this.props.options, data=this.props.data|| this.state.data;     
+    if(!data || data.length==0){
     		if(options.columns){
     			return this.getDataNotFound();
     		}
